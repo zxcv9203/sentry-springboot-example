d97bdbde2f06af179d8751b55d8b00dbbb1b47dc
yongckim <zxcv9203@naver.com>
2023-05-30 01:30:59 +0900

    :tada: 프로젝트 초기화

---
+++ src/test/java/com/example/sentryexample/SentryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sentryexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentryExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
